eaa74bb23b2ad7b2aea385a2df6f197313cbc88b
Leo Lugo <leo.lugo84@gmail.com>
2018-04-12 17:20:16 -0700

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const conn = require('../lib/conn')
+var path = require('path')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Express' });
-});
+	const sql = `
+	SELECT 
+		*
+	FROM 
+		catagories 
+	`
+
+	let data = {
+		title: 'exp'
+	}
+	// conn.connect()
+
+	conn.query(sql, (err, results, fields) => {
+		data.catagories = results.filter(result => result.parent_id === null)
+		data.catagories.map(cat => {
+			let subcat = results.filter( result => {
+				if (result.parent_id === cat.id){
+					return result
+				}
+			})
+			cat.subcatagories = subcat
+		})
+		console.log(data)
+		// res.json(data)
+		res.render('home', data)
+	})
+})
+	
+
+  
 
 module.exports = router;
